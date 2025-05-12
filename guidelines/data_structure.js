a38147039f526af8ae86aefa5766eaf8af7fd0d4
ZakariaZack98 <sadeemdzakaria@yahoo.com>
2025-05-12 14:41:54 +0600

    db & object sructure improvised, query guide added

--- guidelines/data_structure.js
+++ guidelines/data_structure.js
@@ -1,4 +1,4 @@
-//* OBJECT TO STORE AN USER ===========================
+//* OBJECT TO STORE A USER ===========================
 userObject = {
   userId: auth.currentUser.uid,
   username: "johndoe22",
@@ -8,102 +8,113 @@ userObject = {
   gender: "male",
   bio: "Man is both the marble and the sculptor", //? user's bio
   isLocked: false, //? If locked, only shown to friends
-  friendList: {
-    hidden: false, //? If hidden, do not display on profile
-    list: [{ friendObject }, { friendObject }, ...more], //? friendObject containing friend's uid & timestamp of friendship
-  },
-  followingIds: {
-    hidden: false, //? If hidden, do not display on profile
-    list: [uid, uid, ...more], //? array containing following's uid s
-  },
-  followerIds: {
-    hidden: false, //? If hidden, do not display on profile
-    list: [uid, uid, ...more], //? array containing followers uids
-  },
-  blockedIds: [uid, uid, ...more], //? array containing blocked uids
-  blockedByIds: [uid, uid, ...more], //? array containing ids that user is blocked on
+  blockedIds: { uid1: true, uid2: true }, //? object of blocked uids
+  blockedByIds: { uid3: true, uid4: true }, //? object of uids that blocked the user
   defaultTheme: "light", //? user preferred theme will be applied after log-in.
   postCount: 28, //? Total number of active posts
-  socialHandles: [{ socialHandleObj }, { socialHandleObj }, ...more], //? Array of object containing user's other social handles. structure:- {name, url}
+  socialHandles: {
+    facebook: { name: "Facebook", url: "https://fb.com/xyz" },
+    twitter: { name: "Twitter", url: "https://twitter.com/xyz" },
+  }, //? object of user's other social handles
 };
 
-//* OBJECT TO STORE A POST ============================
-postObject = {
-  id: Date.now() + auth.currentUser.uid, //? post's unique ID (combining uid and timestamp)
-  timeStamp: Date.now(), //? timestamp of post creation
-  posterId: "7qdwdy87d98aa8dw8d7wd8", //? comes from auth.currentUser.uid
-  posterName: currentUser.displayName, //? denormalized name
-  posterImgUrl: currentUser.photoUrl, //? denormalized photoUrl
-  createdAt: GetTimeNow(), //? comes from momment JS's time formatter
-  visibility: "public", //? public || private || friends
-  text: "Lorem Ipsum", //? comment's text
-  imgUrls: ["imgUrl", "imgUrl", ...more], //? post's photo urls (if there any)
-  videoUrl: "https://cloudinarystorage.com/fakelink.mp4", //? post's video's url (if there any)
-  likeCounts: 12, //? number of likes
-  comments: [{ commentObject }, { commentObject }, ...more], //? array conatining comment objects,
-  hashtags: ["trendy, recipe, foodchallenge"], //? hashtags in text (should be extracted from post's text)
+//* FLAT OBJECT TO STORE ALL POSTS ====================
+allPosts = {
+  postId: {
+    id: postId,
+    timeStamp: Date.now(),
+    posterId: auth.currentUser.uid,
+    posterName: currentUser.displayName, //? denormalized name
+    posterImgUrl: currentUser.photoUrl, //? denormalized photoUrl
+    createdAt: GetTimeNow(), //? formatted timestamp
+    visibility: "public", //? public || private || friends
+    text: "Lorem Ipsum",
+    imgUrls: ["imgUrl", "imgUrl"],
+    videoUrl: "https://cloudinarystorage.com/fakelink.mp4",
+    likeCounts: 12,
+    hashtags: ["trendy", "recipe", "foodchallenge"],
+  }
 };
 
-//* OBJECT TO UPDATE POST METADATA =====================
-postMetaDataObject = {
-  id: postObject.id, //? same id as the post
-  likesCount: 12, //? number of likes
-  comments: [{ commentObject }, { commentObject }, ...more], //? array conatining comment objects,
+//* FLAT OBJECT TO STORE POST METADATA ===============
+postMetaData = {
+  postId: {
+    likesCount: 12,
+    comments: {
+      commentId1: true,
+      commentId2: true,
+    }, //? references to comment IDs
+  }
 };
 
-//* OBJECT TO STORE A STORY ============================
-storyObject = {
-  id: Date.now() + auth.currentUser.uid, //? post's unique ID (combining uid and timestamp)
-  timeStamp: Date.now(), //? timestamp of post creation
-  posterId: "7qdwdy87d98aa8dw8d7wd8", //? comes from auth.currentUser.uid
-  posterName: currentUser.displayName, //? denormalized name
-  posterImgUrl: currentUser.photoUrl, //? denormalized photoUrl
-  createdAt: GetTimeNow(), //? comes from momment JS's time formatter
-  visibility: "public", //? public || private || friends
-  text: "Lorem Ipsum", //? comment's text
-  imgUrl: "https://dummyimage.com/dummy.png", //? Story's Picture url
-  videoUrl: "https://cloudinarystorage.com/fakelink.mp4", //? post's video's url (if there any)
-  likeCounts: 12, //? number of likes
-  comments: [{ commentObject }, { commentObject }, ...more], //? array conatining comment objects,
+//* FLAT OBJECT TO STORE ALL COMMENTS ================
+allComments = {
+  commentId: {
+    id: commentId,
+    timeStamp: Date.now(),
+    createdAt: GetTimeNow(),
+    postId: "postId", //? link back to post
+    commenterId: auth.currentUser.uid,
+    commenterName: currentUser.displayName,
+    commenterImgUrl: currentUser.photoUrl,
+    text: "Lorem Ipsum",
+    imgUrl: "https://cloudinarystorage.com/fakelink.png",
+    likeCounts: 0,
+    replies: {
+      replyId1: true,
+      replyId2: true,
+    }, //? references to reply IDs
+  }
 };
 
-//* OBJECT TO STORE A COMMENT =========================
-commentObject = {
-  id: Date.now() + auth.currentUser.uid,
-  timeStamp: Date.now(), //? timestamp of post creation //? post's unique ID (combining uid and timestamp)
-  createdAt: GetTimeNow(), //? comes from momment JS's time formatter
-  commenterId: "7qdwdy87d98aa8dw8d7wd8", //? comes from auth.currentUser.uid
-  commenterName: currentUser.displayName, //? denormalized name
-  commenterImgUrl: currentUser.photoUrl, //? denormalized photoUrl
-  text: "Lorem Ipsum", //? comment's text
-  imgUrl: "https://cloudinarystorage.com/fakelink.png", //? commented photo's url (if there any)
-  likeCounts: 0, //? number of likes of the comment
-  replies: [{ replyObject }, { replyObject }, ...more], //? Array of object conataining the comment's replies
+//* FLAT OBJECT TO STORE ALL REPLIES ==================
+allReplies = {
+  replyId: {
+    id: replyId,
+    timeStamp: Date.now(),
+    createdAt: GetTimeNow(),
+    commentId: "commentId", //? link back to comment
+    replierId: auth.currentUser.uid,
+    replierName: currentUser.displayName,
+    replierImgUrl: currentUser.photoUrl,
+    text: "Lorem Ipsum",
+    imgUrl: "https://cloudinarystorage.com/fakelink.png",
+    likeCounts: 0,
+  }
 };
 
-//* OBJECT TO STORE A COMMENT =========================
-replyObject = {
-  id: Date.now() + auth.currentUser.uid, //? post's unique ID (combining uid and timestamp)
-  timeStamp: Date.now(), //? timestamp of post creation
-  createdAt: GetTimeNow(), //? comes from momment JS's time formatter
-  replierId: "7qdwdy87d98aa8dw8d7wd8", //? comes from auth.currentUser.uid
-  replierName: currentUser.displayName, //? denormalized name
-  replierImgUrl: currentUser.photoUrl, //? denormalized photoUrl
-  text: "Lorem Ipsum", //? comment's text
-  imgUrl: "https://cloudinarystorage.com/fakelink.png", //? commented photo's url (if there any)
-  likeCounts: 0, //? number of likes of the comment
-  // NO FURTHER REPLIES
+//* FLAT OBJECT TO STORE STORIES ======================
+allStories = {
+  storyId: {
+    id: storyId,
+    timeStamp: Date.now(),
+    posterId: auth.currentUser.uid,
+    posterName: currentUser.displayName,
+    posterImgUrl: currentUser.photoUrl,
+    createdAt: GetTimeNow(),
+    visibility: "public",
+    text: "Lorem Ipsum",
+    imgUrl: "https://dummyimage.com/dummy.png",
+    videoUrl: "https://cloudinarystorage.com/fakelink.mp4",
+    likeCounts: 12,
+    comments: {
+      commentId1: true,
+      commentId2: true,
+    },
+  }
 };
 
-//* OBJECT TO STORE A NOTIFICATION =====================
-notificationObject = {
-  type: string, //? "like", "comment", "follow", etc.
-  postId: string, // ?ID of the post related to the notification
-  fromUserId: string, //? ID of the user who triggered the notification
-  triggererName: currentUser.displayName, //? denormalized name
-  triggererImgUrl: currentUser.photoUrl, //? denormalized photoUrl
-  toUserId: string, //? ID of the user who received the notification
-  timestamp: number, //? timestamp of when the notification was created
-  read: boolean, //? whether the notification has been read by the user
-  data: object, //? additional data related to the notification (e.g. comment text, like count) *optional
+//* FLAT OBJECT TO STORE NOTIFICATIONS ===============
+notifications = {
+  notificationId: {
+    type: "like", //? or "comment", "follow", etc.
+    postId: "postId",
+    fromUserId: "userId",
+    triggererName: currentUser.displayName,
+    triggererImgUrl: currentUser.photoUrl,
+    toUserId: "userId",
+    timestamp: Date.now(),
+    read: false,
+    data: { text: "optional comment text", likeCount: 1 },
+  }
 };
--- guidelines/db_strucure.md
+++ guidelines/db_strucure.md
@@ -3,70 +3,128 @@
     |-username
     |-email
     |-imgUrl
+    |-bio
+    |-blockedIds/
+      |-uid1: true
+      |-uid2: true
+    |-blockedByIds/
+      |-uid3: true
+      |-uid4: true
+    |-socialHandles/
+      |-facebook/
+        |-name: "Facebook"
+        |-url: "https://fb.com/xyz"
+      |-twitter/
+        |-name: "Twitter"
+        |-url: "https://twitter.com/xyz"
     ...()
-  |-userId/
-  ...
-  |-userId/
-  ...
--posts/
-  |-userId/(all the posts of the user (which one the userId belongs) will be stored here)
-    |-postId/(contains post objects)
-      |-createdAt
-      |-posterId
-      |-text
-      ...
-    |-postId
-      ...
-    |-postId
-      ...
-  |-userId/(another user)
-    |-postId
-    ...
-  |-userId/(another user)
-    |-postId
-    ...
+
+-allPosts/
+  |-postId/ (contains flat post object)
+    |-createdAt
+    |-posterId
+    |-posterName
+    |-posterImgUrl
+    |-visibility
+    |-text
+    |-imgUrls/
+      |-0: "imgUrl1"
+      |-1: "imgUrl2"
+    |-videoUrl
+    |-hashtags/
+      |-0: "trendy"
+      |-1: "recipe"
+    |-likeCounts
+
 -postMetaData/
   |-postId/
-    |-id
+    |-likesCount
+    |-comments/
+      |-commentId1: true
+      |-commentId2: true
+
+-allComments/
+  |-commentId/
+    |-postId
+    |-commenterId
+    |-commenterName
+    |-commenterImgUrl
+    |-text
+    |-imgUrl
+    |-likeCounts
+    |-replies/
+      |-replyId1: true
+      |-replyId2: true
+
+-allReplies/
+  |-replyId/
+    |-commentId
+    |-replierId
+    |-replierName
+    |-replierImgUrl
+    |-text
+    |-imgUrl
+    |-likeCounts
+
+-allStories/
+  |-storyId/
+    |-createdAt
+    |-posterId
+    |-posterName
+    |-posterImgUrl
+    |-visibility
+    |-text
+    |-imgUrl
+    |-videoUrl
     |-likeCounts
     |-comments/
-      |-commentId/
-        |-commenterId
-        |-text
-        |-replies/
-          |-replyId/
-            |-replierId
-            |-text
-            |-timeStamp
-            ...
-          |-replyId/
-           ...
-          |-replyId/
-           ...
-      |-commentId/
-        ...
-      |-commentId/
-        ...
-  |-postId/
-    ...
-  |-postId/
-    ...
--stories/
-  |-userId
-    |-id (contains)
-    ........all similiar structure as posts/
+      |-commentId1: true
+      |-commentId2: true
+
 -notifications/
-  |-userId/
-    |-notificationId/
-     |-timeStamp
-     |-type
-     |-postId
-     ...
-    |-notificationID/
-     ...
-    |-notificationID/
-     ...
-  |-userId/
-    ...
-  |-userId/
-    ...
+  |-notificationId/
+    |-timestamp
+    |-type
+    |-postId
+    |-fromUserId
+    |-triggererName
+    |-triggererImgUrl
+    |-toUserId
+    |-read
+    |-data/
+      |-text
+      |-likeCount
+
+-followers/
+  |-userId/ (this user is being followed by these uids)
+    |-followerId1: true
+    |-followerId2: true
+    |-followerId3: true
+
+-following/
+  |-userId/ (this user is following these uids)
+    |-followingId1: true
+    |-followingId2: true
+    |-followingId3: true
+
+
+================================== QWERY GUIDE ===================================
+IE.: Get all post from a certain user
+
+
+const postsRef = ref(db, 'allPosts');
+const userPostsQuery = query(postsRef, orderByChild('posterId'), equalTo('user123'));
+
+get(userPostsQuery).then(snapshot => {
+  if (snapshot.exists()) {
+    const posts = snapshot.val();
+    console.log("User's Posts:", posts);
+  } else {
+    console.log("No posts found for this user.");
+  }
+});
+
+** orderByChild: the key name under our query factor is stored (this case, 'posterId')
+** equalTo: the value we want to match with our desired data (this case, 'user123' which belongs to the user whose posts we are quering)
+
+(READ DOCS FOOR MORE INFO)

1f08652e074942196895f79215d47d4e13be1812
Mahmud-Dev63828 <mahmudul.hasan63828@gmail.com>
2025-05-13 19:53:29 +0600

    sidebar menu ui done

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { ref, onValue, off } from "firebase/database";
 import {
   AiFillHome,
   AiOutlineSearch,
@@ -11,10 +12,16 @@ import { MdOutlineOndemandVideo } from "react-icons/md";
 import { FiSend } from "react-icons/fi";
 import { CgProfile } from "react-icons/cg";
 import { BiMenu } from "react-icons/bi";
+import SidebarMenu from "./SidebarMenu";
+import { auth } from "../../../Database/Firebase.config";
+import { db } from "../../../Database/Firebase.config";
 
 const Sidebar = () => {
   const location = useLocation();
+  const [currentUseData, setCurrentUseData] = useState([]);
   const navigate = useNavigate();
+  const [showSidebarMenu, setShowSidebarMenu] = useState(false);
+
   const navItems = [
     { label: "Home", icon: AiFillHome, path: "/" },
     { label: "Search", icon: AiOutlineSearch, path: "/search" },
@@ -26,10 +33,23 @@ const Sidebar = () => {
     { label: "Profile", icon: CgProfile, path: "/profile" },
     { label: "More", icon: BiMenu, path: "/more" },
   ];
+  // useEffect(() => {
+  //   const uid = auth.currentUser?.uid;
+  //   if (!uid) return;
+  //   const userRef = ref(db, `users/${uid}`);
+  //   onValue(userRef, (snapshot) => {
+  //     if (snapshot.exists()) {
+  //       setCurrentUseData(snapshot.val());
+  //     }
+  //   });
+  // }, []);
+  // console.log(currentUseData);
+
   return (
     <div className="w-1/5 h-screen overflow-hidden p-5 border-r border-gray-300">
       <div className="w-64 h-full p-4 flex flex-col">
         <h1 className="text-2xl font-bold mb-10 ml-2 font-sans">Instagram</h1>
+
         <nav className="flex flex-col gap-2">
           {navItems.map(({ label, icon: Icon, path }, index) => {
             const isActive = location.pathname === path;
@@ -38,16 +58,27 @@ const Sidebar = () => {
             return (
               <div
                 key={label}
-                onClick={() => navigate(path)}
+                onClick={() => {
+                  if (isLast) {
+                    setShowSidebarMenu((prev) => !prev);
+                  } else {
+                    navigate(path);
+                  }
+                }}
                 className={`flex items-center gap-4 px-4 py-2 rounded-lg cursor-pointer transition-all
-              ${isActive ? "font-semibold bg-gray-100" : "hover:bg-gray-100"}
-              ${isLast ? "mt-8" : ""}`}
+        ${isActive ? "font-semibold bg-gray-100" : "hover:bg-gray-100"}
+        ${isLast ? "mt-8 relative" : ""}`}
               >
                 <Icon size={24} />
                 <span>{label}</span>
               </div>
             );
           })}
+          {showSidebarMenu && (
+            <div className="absolute z-1000">
+              <SidebarMenu />
+            </div>
+          )}
         </nav>
       </div>
     </div>
---
+++ src/components/common/SidebarMenu.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  FiSettings,
+  FiActivity,
+  FiBookmark,
+  FiSun,
+  FiAlertCircle,
+  FiLogOut,
+  FiRepeat,
+  FiUser,
+} from "react-icons/fi";
+import { FaThreads } from "react-icons/fa6";
+
+const SidebarMenu = () => {
+  const menuItems = [
+    { label: "Settings", icon: <FiSettings />, path: "/settings" },
+    { label: "Your activity", icon: <FiActivity />, path: "/activity" },
+    { label: "Saved", icon: <FiBookmark />, path: "/saved" },
+    { label: "Switch appearance", icon: <FiSun />, path: "/appearance" },
+    { label: "Report a problem", icon: <FiAlertCircle />, path: "/report" },
+    { label: "Threads", icon: <FaThreads />, path: "/threads" },
+    { label: "Switch accounts", icon: <FiUser />, path: "/switch-accounts" },
+    { label: "Log out", icon: <FiLogOut />, path: "/logout" },
+  ];
+  const navigate = useNavigate();
+
+  return (
+    <div
+      style={{ boxShadow: "0 0 5px 5px rgba(0,0,0,0.1)" }}
+      className="w-56 bg-white  rounded-xl p-2 space-y-1"
+    >
+      {menuItems.map((item, index) => (
+        <div
+          key={index}
+          onClick={() => navigate(item.path)}
+          className="flex items-center gap-3 p-3 rounded-lg cursor-pointer hover:bg-gray-100 transition-all"
+        >
+          <span className="text-xl">{item.icon}</span>
+          <span className="text-sm font-medium">{item.label}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SidebarMenu;

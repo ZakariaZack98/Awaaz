eb01cb68887ecf9fe73045fb7f747e53bca253f0
ZakariaZack98 <sadeemdzakaria@yahoo.com>
2025-05-15 23:11:25 +0600

    postcard all functionality ready

--- src/components/home/PostCard.jsx
+++ src/components/home/PostCard.jsx
@@ -7,20 +7,44 @@ import { FaBookmark, FaHeart, FaRegBookmark, FaRegHeart } from "react-icons/fa";
 import { IoChatbubbleOutline } from "react-icons/io5";
 import { PiPaperPlaneTilt } from "react-icons/pi";
 import { CiFaceSmile } from "react-icons/ci";
-import EmojiPicker from 'emoji-picker-react';
-import { auth } from "../../../Database/Firebase.config";
+import EmojiPicker from "emoji-picker-react";
+import { auth, db } from "../../../Database/Firebase.config";
+import moment from "moment";
+import { AddComment, CheckIfLiked, CheckIfSaved, FetchLikesCommentsCount, LikePost, RemoveSavedPost, SavePost, UnlikePost } from "../../utils/actions.utils";
+import { toast } from "react-toastify";
 
-const PostCard = () => {
-  // const {id, text, poseterId, posterName, posterImgUrl, createdAt, imgUrls, videoUrl, likeCounts} = postData;
+const PostCard = ({ postData }) => {
+  const { id, text, posterUsername, posterId, posterName, posterImgUrl, createdAt, imgUrls, videoUrl } = postData;
   const [openPostActions, setOpenPostActions] = useState(false);
   const [liked, setLiked] = useState(false);
   const [saved, setSaved] = useState(false);
   const [likesCount, setLikesCount] = useState(12);
   const [commentsCount, setCommentsCount] = useState(28);
-  const [comment, setComment] = useState('');
-  const [displayComment, setDisplayComment] = useState('');
+  const [comment, setComment] = useState("");
+  const [displayComment, setDisplayComment] = useState("");
   const [showEmojiPicker, setShowEmojiPicker] = useState(false);
   const emojiPickerRef = useRef(null);
+
+  // TODO: FETCH POST METADATA ==================================
+  useEffect(() => {
+  Promise.all([
+    FetchLikesCommentsCount(id),
+    CheckIfLiked(id),
+    CheckIfSaved(id)
+  ])
+  .then(([data, liked, saved]) => {
+    const [likes, comments] = data;
+    if(likes) {
+      setLikesCount(likes)
+    } else setLikesCount(0)
+    if(comments) {
+      setCommentsCount(comments)
+    } else setCommentsCount(0)
+    setLiked(liked)
+    setSaved(saved)
+  })
+  .catch(err => console.error(err))
+}, [])
 
   // TODO: CLOSE EMOJI PICKER WHEN CLICKED OUTSIDE ==============
   useEffect(() => {
@@ -30,38 +54,49 @@ const PostCard = () => {
       }
     };
 
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
   const handleEmojiClick = (emojiObject) => {
-    setComment(prevComment => prevComment + emojiObject.emoji);
+    setComment((prevComment) => prevComment + emojiObject.emoji);
   };
 
   const toggleEmojiPicker = () => {
-    setShowEmojiPicker(prev => !prev);
+    setShowEmojiPicker((prev) => !prev);
   };
 
   // TODO: DISPLAY COMMENT ON POSTCARD & POST COMMENT TO POST DB
-  const handleComment = (comment, postId) => {
-    setDisplayComment(comment);
-    setComment('');
-    setCommentsCount(commentsCount + 1)
-    //* comment post function here
-  }
+  const handleComment = async (postId) => {
+    if(comment.length === 0) {
+        toast.error(`Can't post empty comment.`);
+        return;
+    }
+    try {
+      await AddComment(postId, comment);
+      setDisplayComment(comment);
+      setCommentsCount(commentsCount + 1);
+    } catch (error) {
+      console.error('Error posting comment', error.message)
+    } finally {
+      setComment("");
+    }
+  };
 
   // TODO: HANDLE POST LIKE ====================================
-  const handleLike = postId => {
-    setLiked(prev => !prev);
-    //* like post function here
-  }
+  const handleLike = async () => {
+    liked ? setLikesCount(likesCount - 1) : setLikesCount(likesCount + 1)
+    setLiked(liked ? false : true);
+    liked ? await UnlikePost(id) : await LikePost(id)
+  };
+  
   // TODO: HANDLE POST SAVE ====================================
-  const handleSave = postId => {
-    setSaved(prev => !prev);
-    //* save post function here
-  }
+  const handleSave = async () => {
+    setSaved(saved ? false : true);
+    saved ? await RemoveSavedPost(id) : await SavePost(id)
+  };
 
   return (
     //================ HEADING ======================//
@@ -70,20 +105,20 @@ const PostCard = () => {
         <div className="flex items-center gap-x-2 cursor-pointer">
           <img
             src={
+              posterImgUrl ||
               "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"
             }
-            alt={""}
             className="rounded-full w-10 h-10"
           />
           <div className="flex items-start gap-x-2">
             <div className="flex flex-col">
-              <p className="text-sm font-semibold">{"Sadee Muhammad Zakaria"}</p>
-              <p className="opacity-70 text-[13px]">{"@username"}</p>
+              <p className="text-sm font-semibold">{posterName || "Name Missing"}</p>
+              <p className="opacity-70 text-[13px]">{posterUsername ? `@${posterUsername}` : "@username"}</p>
             </div>
             <span>
               <GoDotFill />
             </span>
-            <p className="opacity-70 text-[13px]">{"2 hours ago"}</p>
+            <p className="opacity-70 text-[13px]">{moment(createdAt).fromNow()}</p>
           </div>
         </div>
         <span className="relative">
@@ -95,18 +130,33 @@ const PostCard = () => {
           )}
         </span>
       </div>
-      {/* ========== IMAGE SLIDER =========== */}
-      <div className="media">
-        <ImageSlider />
-      </div>
+      {/* ========== MEDIA PART =========== */}
+      {imgUrls && imgUrls.length > 1 && (
+        <div className="media">
+          <ImageSlider imgUrlArray={imgUrls} />
+        </div>
+      )}
+      {
+        imgUrls && imgUrls.length === 1 && (
+          <picture className="w-full">
+            <img src={imgUrls[0]} alt="" className="w-full object-cover object-center"/>
+          </picture>
+        )
+      }
+      {
+        videoUrl && videoUrl.length > 0 && (
+          <video src={videoUrl} controls className="w-full"></video>
+        )
+      }
       {/* ========== ICONS ================== */}
       <div className="icons flex justify-between py-3 text-2xl">
         <div className="flex items-center gap-x-4">
-          <span onClick={() => {
-            setLiked(!liked);
-            setLikesCount(liked ? likesCount - 1 : likesCount + 1);
-          }}>
-            {liked ? <FaHeart className="text-red-600 cursor-pointer" onClick={() => handleLike()}/> : <FaRegHeart className="cursor-pointer" onClick={() => handleLike()}/>}
+          <span>
+            {liked ? (
+              <FaHeart className="text-red-600 cursor-pointer" onClick={() => handleLike()} />
+            ) : (
+              <FaRegHeart className="cursor-pointer" onClick={() => handleLike()} />
+            )}
           </span>
           <span>
             <IoChatbubbleOutline className=" cursor-pointer" />
@@ -117,39 +167,34 @@ const PostCard = () => {
         </div>
         <span onClick={() => setSaved(!saved)}>
           {saved ? (
-            <FaBookmark className="text-red-600 cursor-pointer" onClick={() => handleSave()}/>
+            <FaBookmark className="text-red-600 cursor-pointer" onClick={() => handleSave()} />
           ) : (
-            <FaRegBookmark className="cursor-pointer" onClick={() => handleSave()}/>
+            <FaRegBookmark className="cursor-pointer" onClick={() => handleSave()} />
           )}
         </span>
       </div>
       {/* ============= LIKES & COMMENTS ============== */}
       <p className="font-semibold text-sm">{likesCount} likes</p>
-      <span className="font-semibold text-sm cursor-pointer">{"Poster Name"}</span>
-      <p className="postcardCaption text-sm text-black">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, expedita omnis officiis reprehenderit neque
-        totam vitae laborum odio iure obcaecati.
-      </p>
-      <p className=" text-sm mt-2 cursor-pointer">View all {commentsCount} comments...</p>
-      {
-        displayComment && displayComment.length > 0 && (
-          <div className="flex items-center gap-x-2">
-            <img src={auth.currentUser.photoURL} className="w-5 h-5 rounded-full" />
-            <p className="text-sm font-semibold">{auth.currentUser.displayName}</p>
-            <p className="text-sm py-1">{displayComment}</p>
-          </div>
-        )
-      }
-      <div className="comment flex pb-2 pt-1 justify-between border-b border-[rgba(0,0,0,0.18)] relative">
-        <input 
-          type="text" 
-          value={comment} 
-          placeholder="Add a comment" 
-          onChange={e => setComment(e.target.value)} 
+      <span className="font-semibold text-sm cursor-pointer">{posterName || "Poster Name"}</span>
+      <p className="postcardCaption text-sm text-black">{text}</p>
+      {commentsCount > 0 && <p className=" text-sm mt-2 cursor-pointer">View all {commentsCount} comments...</p>}
+      {displayComment && displayComment.length > 0 && (
+        <div className="flex items-center gap-x-2">
+          <img src={auth.currentUser.photoURL} className="w-5 h-5 rounded-full" />
+          <p className="text-sm font-semibold">{auth.currentUser.displayName}</p>
+          <p className="text-sm py-1">{displayComment}</p>
+        </div>
+      )}
+      <div className="comment flex pb-2 pt-4 justify-between border-b border-[rgba(0,0,0,0.18)] relative">
+        <input
+          type="text"
+          value={comment}
+          placeholder="Add a comment"
+          onChange={(e) => setComment(e.target.value)}
           className="w-9/10 text-sm focus:outline-0"
-          onKeyDown={e => {
-            if(e.key === 'Enter') {
-              handleComment(e.target.value)
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleComment(id);
             }
           }}
         />
@@ -158,11 +203,7 @@ const PostCard = () => {
         </span>
         {showEmojiPicker && (
           <div className="absolute bottom-10 right-0 z-50" ref={emojiPickerRef}>
-            <EmojiPicker 
-              onEmojiClick={handleEmojiClick} 
-              width={300}
-              height={350}
-            />
+            <EmojiPicker onEmojiClick={handleEmojiClick} width={300} height={350} />
           </div>
         )}
       </div>
